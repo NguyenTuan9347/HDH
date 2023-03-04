16538e491147e95a46db88842f678ae45c204ffd
Phan Huy Đức Tài <phanhuyductai@gmail.com>
2023-03-05 02:27:34 +0700

    Update đã đọc được tên, loại và size file

---
+++ HDH.h
@@ -0,0 +1,151 @@
+﻿    #pragma once
+#include <cstdio>
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <vector>
+#include <fcntl.h>
+#include <io.h>
+using namespace std;
+
+#define bytePerSectorIndex 11 // size la 2
+#define SectorPerCluster 13 // size la 1 // Sectors Per Cluster
+#define ReservedSectors 14 // size la 2 //Reserved Sectors
+#define SectorPerTrack 24 // size 2
+#define numberOfHead 26 // size la 2
+#define TotalSectors 40// size 8
+#define LogicalClusterNumber 48 // size 8//Logical Cluster Number for the file $MFT
+#define LogicalClusterNumberforthefile$MFTMirr 56 // size 8//Logical Cluster Number for the file $MFTMirr
+#define ClustersPerFileRecordSegment 64 // size 4
+#define ClustersPerIndexBuffer 68//size 1
+#define ScIndex 13 // size la 1 // sector per cluster
+#define SbIndex 14 // size la 2 // sector per boot sector
+#define nFIndex 16 // size la 1 // number of fat32 table
+#define SectorperTrackIndex 24 // size 2 // sector per track
+#define numberOfHeadIndex 26 // size la 2 
+#define SvIndex 32 // size 4 // size of volume
+#define SfIndex 36 // size 4 // size of fat table
+#define RDETstartIndex 44 // size 4 // started RDET index
+#define sectorEmptyClusterIndex 48 // size 2
+#define sectorBackUpBSIndex 50 // size 2    
+
+unsigned int charToInt(unsigned char* arr, int size);
+int ReadSector(LPCWSTR  drive, int readPoint, unsigned char sector[512]);
+void drawMenu();
+void quanlywindow();
+void printfNtfs();
+void prinfFat32();
+void GotoXY(int x, int y);
+
+
+class NTFS {
+public:
+    int bpS, SpC, RS, nH, SpT, TotalSec, LCN, LCN_2, CFRS, CIB;
+    NTFS(unsigned char sector[]) {
+        bpS = charToInt(&sector[bytePerSectorIndex], 2);
+        SpC = charToInt(&sector[SectorPerCluster], 1);
+        RS = charToInt(&sector[ReservedSectors], 2);
+        nH = charToInt(&sector[numberOfHead], 2);
+        SpT = charToInt(&sector[SectorPerTrack], 2);//Sectors Per Track
+        TotalSec = charToInt(&sector[TotalSectors], 8);
+        LCN = charToInt(&sector[LogicalClusterNumber], 8);//Logical Cluster Number for the file $MFT 
+        LCN_2 = charToInt(&sector[LogicalClusterNumberforthefile$MFTMirr], 8);//Logical Cluster Number for the file $MFTMirr 
+        CFRS = charToInt(&sector[ClustersPerFileRecordSegment], 4);//Clusters Per File Record Segment
+        CIB = charToInt(&sector[ClustersPerIndexBuffer], 1);//Clusters Per Index Buffer
+    }
+};
+
+class FAT32 {
+public:
+    int Sf, Sb, Sc, nF, clusterRDETFirstIndex, bytePerSector, Sv, nH;
+    FAT32(unsigned char sector[]) {
+        bytePerSector = charToInt(&sector[bytePerSectorIndex], 2);
+        Sc = charToInt(&sector[ScIndex], 1);
+        Sb = charToInt(&sector[SbIndex], 2);
+        nF = charToInt(&sector[nFIndex], 1);
+        nH = charToInt(&sector[numberOfHeadIndex], 2);
+        Sv = charToInt(&sector[SvIndex], 4);
+        Sf = charToInt(&sector[SfIndex], 4);
+        clusterRDETFirstIndex = charToInt(&sector[RDETstartIndex], 4);
+    }
+    int byteStartOfRDET() {
+        return (nF * Sf + Sb) * bytePerSector;
+    }
+
+};
+NTFS* readNTFS(LPCWSTR path);
+FAT32* readFAT32(LPCWSTR path);
+class Component
+{
+protected:
+	Component* parent = nullptr;
+	string name, extension;
+	long size = 0;
+public:
+	virtual string getName() = 0;
+	virtual string getExtension() = 0;
+	virtual long getSize() = 0;
+	virtual void AddComponent(Component* obj)
+	{
+
+	}
+	virtual void RemoveComponent(Component* obj)
+	{
+
+	}
+	virtual ~Component() {
+		if (parent != nullptr) delete parent;
+	}
+};
+class Folder : public Component
+{
+	vector<Component*> components;
+public:
+	string getName() { return name; }
+	string getExtension() { return ""; }
+	long getSize() {
+		for (int i = 0; i < components.size(); ++i)
+		{
+			size += components[i]->getSize();
+		}
+		return size;
+	}
+	void AddComponent(Component* obj)
+	{
+		if (obj != NULL)
+		{
+			components.push_back(obj);
+		}
+	}
+	~Folder()
+	{
+		for (int i = 0; i < components.size(); ++i)
+		{
+			delete components[i];
+		}
+		components.clear();
+	}
+};
+class File : public Component
+{
+public:
+	File(string namePara, string extensionPara) { name = namePara; extension = extensionPara; }
+	string getName() { return name; }
+	string getExtension() { return extension; }
+	void setSize(int value)
+	{
+		size = value;
+	}
+	long getSize()
+	{
+		return size;
+	}
+};
+
+class RDET
+{
+public:
+	void handleFakeEntries(LPCWSTR  drive, int readPoint, unsigned char sector[512], int checkValid, string& fullName);
+	void readEntries(LPCWSTR  drive, int readPoint);
+};
---
+++ readBoot.cpp
@@ -0,0 +1,136 @@
+﻿#include "HDH.h"
+
+NTFS* readNTFS(LPCWSTR path) {
+    unsigned char sector[512];
+    ReadSector(path, 0, sector);
+    printf("Bytes Per Sector : %u \n", charToInt(&sector[bytePerSectorIndex], 2));
+    printf("Cluster per sector : %u \n", charToInt(&sector[SectorPerCluster], 1));
+    printf("Reserved Sectors : %u \n", charToInt(&sector[ReservedSectors], 2));
+    printf("Sectors Per Track : %u \n", charToInt(&sector[SectorPerTrack], 2));
+    printf("Number Of Heads : %u \n", charToInt(&sector[numberOfHead], 2));
+    printf("Total Sectors : %u \n", charToInt(&sector[TotalSectors], 8));
+    printf("Logical Cluster Number for the file $MFT : %u \n", charToInt(&sector[LogicalClusterNumber], 8));
+    printf("Logical Cluster Number for the file $MFTMirr : %u \n", charToInt(&sector[LogicalClusterNumberforthefile$MFTMirr], 8));
+    printf("Clusters Per File Record Segment : %u \n", charToInt(&sector[ClustersPerFileRecordSegment], 4));
+    printf("Clusters Per Index Buffer : % u \n", charToInt(&sector[ClustersPerIndexBuffer], 1));
+    printf("\n");
+    return new NTFS(sector);
+}
+
+FAT32* readFAT32(LPCWSTR path) {
+    unsigned char sector[512];
+    ReadSector(path, 0, sector);
+    printf("Byte per sector : %u \n", charToInt(&sector[bytePerSectorIndex], 2));
+    printf("Cluster per sector : %u \n", charToInt(&sector[ScIndex], 1));
+    printf("Sector per Bootsector %u \n", charToInt(&sector[SbIndex], 2));
+    printf("Number of FAT table : %u \n", charToInt(&sector[nFIndex], 1));
+    printf("Sector per track : %u \n", charToInt(&sector[SectorperTrackIndex], 2));
+    printf("Number of head : %u \n", charToInt(&sector[numberOfHeadIndex], 2));
+    printf("Volume size : %u \n", charToInt(&sector[SvIndex], 4));
+    printf("FAT table size : %u \n", charToInt(&sector[SfIndex], 4));
+    printf("Cluster staring of RDET : %u \n", charToInt(&sector[RDETstartIndex], 4));
+    printf("Empty cluster info sector : %u \n", charToInt(&sector[sectorEmptyClusterIndex], 2));
+    printf("Backup bootsector : %u \n", charToInt(&sector[sectorBackUpBSIndex], 2));
+    printf("Type of FAT : ");
+    for (int i = 82; i < 90; i++) {
+        printf("%c", sector[i]);
+    }
+    printf("\n");
+    return new FAT32(sector);
+}
+
+void RDET::handleFakeEntries(LPCWSTR  drive, int readPoint, unsigned char sector[512], int checkValid, string& fullName)
+{
+	fullName += "-FakeEntry-";
+	int indexByte = 0;
+	checkValid -= 32;
+	while (sector[checkValid] == 15)
+	{
+		indexByte = checkValid - 11 + 1;
+		cout << "Found a fake entry" << endl;
+		for (int i = 0; i < 10; ++i)
+		{
+			fullName += (char)(charToInt(&sector[indexByte], 1));
+			indexByte++;
+		}
+		indexByte = checkValid - 11 + 14;
+		for (int i = 0; i < 12; ++i)
+		{
+			fullName += (char)(charToInt(&sector[indexByte], 1));
+			indexByte++;
+		}
+		indexByte = checkValid - 11 + 28;
+		for (int i = 0; i < 4; ++i)
+		{
+			fullName += (char)(charToInt(&sector[indexByte], 1));
+			indexByte++;
+		}
+		if (checkValid - 32 < 0)
+		{
+			ReadSector(drive, readPoint - 512, sector);
+			checkValid = checkValid - 32 + 512;
+		}
+		else checkValid -= 32;
+	}
+}
+void RDET::readEntries(LPCWSTR  drive, int readPoint)
+{
+	unsigned char sector[512];
+	ReadSector(drive, readPoint, sector);
+	int checkValid = 11; //Kiểm tra có phải là file, folder hay Read-Only không
+	int indexByte;
+	string fullName;
+	string fileExtension;
+	int objSize = 0;
+	//int count = 0;
+	while (checkValid < 512)
+	{
+		if (sector[checkValid] == 32)
+		{
+			//File không có entry phụ
+			indexByte = checkValid - 11;
+			//Lấy tên từ 8 byte đầu
+			for (int i = 0; i < 8; ++i)
+			{
+				fullName.push_back((char)sector[indexByte]);
+				indexByte++;
+			}
+			//Lấy phần mở rộng trong 3 byte sau
+			for (int j = 0; j < 3; ++j)
+			{
+				fileExtension.push_back((char)sector[indexByte]);
+				indexByte++;
+			}
+			//Lấy kích cỡ object
+			indexByte = checkValid - 11 + 28;
+			objSize = charToInt(&sector[indexByte], 4);
+
+
+			//Kiểm tra file có entry phụ nằm ở sector khác
+			if (checkValid - 32 < 0)
+			{
+				cout << "Found fake entry in another sector" << endl;
+				readPoint -= 512;
+				ReadSector(drive, readPoint, sector);
+				checkValid = checkValid + 512;
+			}
+			//Trường hợp có entry phụ
+			if (sector[checkValid - 32] == 15)
+			{
+				//fullName.erase();
+				handleFakeEntries(drive, readPoint, sector, checkValid, fullName);
+				cout << fullName << endl;
+			}
+		}
+		else if (sector[checkValid] == 16)
+		{
+			//Folder
+		}
+		else if (sector[checkValid] == 1)
+		{
+			//Read-only
+		}
+		checkValid += 32;
+	}
+	system("pause");
+}
